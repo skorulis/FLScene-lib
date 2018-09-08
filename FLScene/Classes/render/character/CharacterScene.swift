fba332d4248befd5e369d14c86d62e0cf14d5e08
Alexander Skorulis <skorulis@gmail.com>
2018-09-08 13:56:24 +1000

    Add a single character into the scene

--- FLScene/Classes/render/character/CharacterScene.swift
+++ FLScene/Classes/render/character/CharacterScene.swift
@@ -7,17 +7,21 @@
 
 import SceneKit
 
-public class CharacterScene: BaseScene {
+public class CharacterScene: BaseScene, MapSceneProtocol {
 
     let island:DungeonModel
     let islandNode:MapIslandNode
     var playerEntity:GridEntity!
+    var character:CharacterModel
+    
+    var characterManager:CharacterManager!
     
     public init(island:DungeonModel,character:CharacterModel) {
         self.island = island
+        self.character = character
         self.islandNode = MapIslandNode(dungeon: self.island,gridSpacing:2.0)
         super.init()
-        
+        self.characterManager = CharacterManager(spellManager: nil,scene:self)
         buildScene()
     }
     
@@ -33,6 +37,34 @@ public class CharacterScene: BaseScene {
         let act1 = SCNAction.moveBy(x: 0, y: -0.5, z: 0, duration: 6)
         let act2 = SCNAction.moveBy(x: 0, y: 0.5, z: 0, duration: 6)
         islandNode.runAction(SCNAction.repeatForever(SCNAction.sequence([act1,act2])))
+        
+        playerEntity = makeEntity(character: character, playerNumber: 1, position: vector_int2(0,0))
+    }
+    
+    func makeEntity(character:CharacterModel,playerNumber:Int,position:vector_int2) -> GridEntity {
+        let location = LocationModel(gridPosition: position, islandName: "battle")
+        let entity = GridEntity(location: location)
+        characterManager.addSprite(entity: entity, imageNamed: character.spriteName)
+        
+        return entity
+    }
+    
+    //MARK: MapSceneProtocol
+    
+    public func pointFor(position:vector_int2,inDungeon dungeon:DungeonModel) -> SCNVector3 {
+        return islandNode.topPosition(at: position)
+    }
+    
+    public func island(named:String) -> DungeonModel {
+        return self.island
+    }
+    
+    public func islandFor(dungeon:DungeonModel) -> MapIslandNode {
+        return self.islandNode
+    }
+    
+    public var bridges:BridgeContainerNode {
+        return BridgeContainerNode()
     }
     
 }
